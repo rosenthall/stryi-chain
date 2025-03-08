907cbe1722655e4856a4067e39809a45c9162fa6
rzntl <rosenthall@riseup.net>
2025-03-08 20:15:10 +0200

    Designed and added gRPC .proto file for blocks synchronization. Added build.rs with tonic_build to build gRPC

---
+++ crates/stryi_network/build.rs
@@ -0,0 +1,9 @@
+use tonic_build::compile_protos;
+
+fn main() -> Result<(), Box<dyn std::error::Error>> {
+    println!("cargo:rerun-if-changed=proto/");
+    
+    compile_protos("proto/sync.proto")?;
+
+    Ok(())
+}
